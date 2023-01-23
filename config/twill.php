6bd3dcfc291445845ba6be1d68db975b9742bde3
Vladimir Pejic <vladimir.pejic@gmail.com>
2023-01-23 19:22:34 +0100

    first commit

---
+++ config/twill.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'enabled' => [
+        'users-management' => true,
+        'media-library' => true,
+        'file-library' => true,
+        'block-editor' => true,
+        'buckets' => true,
+        'users-image' => false,
+        'settings' => true,
+        'dashboard' => true,
+        'search' => true,
+        'users-description' => false,
+        'activitylog' => true,
+        'users-2fa' => false,
+        'users-oauth' => false,
+    ],
+];
